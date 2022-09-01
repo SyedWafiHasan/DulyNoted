53b2dee0ee40e484ea29e5e0184bd21b7ce6bed3
Syed Wafi Hasan <syedwafihasan@gmail.com>
2022-09-01 23:02:37 +0530

    Added TextFields and TextButton to registration page

--- dulynoted/lib/main.dart
+++ dulynoted/lib/main.dart
@@ -5,16 +5,42 @@ void main()
   runApp(MaterialApp(
     title: 'Duly Noted',
     theme: ThemeData(
-      primarySwatch: Colors.green,
+      primarySwatch: Colors.amber,
     ),
     home: const MyHomePage(),
   ));
 }
 
 //Creating a stateless homepage
-class MyHomePage extends StatelessWidget
+class MyHomePage extends StatefulWidget
 {
   const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+{
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState()
+  {
+    _email = TextEditingController();
+    _password = TextEditingController();
+
+    super.initState();
+  }
+
+  @override
+  void dispose()
+  {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context)
@@ -23,6 +49,29 @@ class MyHomePage extends StatelessWidget
       appBar: AppBar(
         title: const Text("Register User"),
       ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            decoration: const InputDecoration(
+              hintText: "Email",
+            ),
+          ),
+          TextField(
+            controller: _password,
+            decoration: const InputDecoration(
+              hintText: "Password",
+            ),
+          ),
+          TextButton(
+            onPressed: () async
+            {
+              print("Button pressed");
+            },
+            child: const Text("Ye hai ek button"),
+            ),
+          ],
+        )
     );
   }
 }
