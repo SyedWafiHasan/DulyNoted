3caecf1ec05563f5dabbc86f5af01d52fff13e99
Syed Wafi Hasan <syedwafihasan@gmail.com>
2022-09-02 13:32:58 +0530

    Reorganized code

--- dulynoted/lib/main.dart
+++ dulynoted/lib/main.dart
@@ -12,20 +12,21 @@ void main()
     theme: ThemeData(
       primarySwatch: Colors.teal,
     ),
-    home: const MyHomePage(),
+    home: const RegisterView(),
   ));
 }
 
-//Creating a stateless homepage
-class MyHomePage extends StatefulWidget
+// creating Stateful RegisterView widget
+// this will be used as the base of our register view
+class RegisterView extends StatefulWidget
 {
-  const MyHomePage({Key? key}) : super(key: key);
+  const RegisterView({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<RegisterView> createState() => _RegisterViewState();
 }
 
-class _MyHomePageState extends State<MyHomePage>
+class _RegisterViewState extends State<RegisterView>
 {
   late final TextEditingController _email;
   late final TextEditingController _password;
@@ -51,66 +52,66 @@ class _MyHomePageState extends State<MyHomePage>
   Widget build(BuildContext context)
   {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Register User"),
-      ),
+        appBar: AppBar(
+          title: const Text("Register User"),
+        ),
 
-      // using FutureBuilder so that the widget does not load until Firebase has initialized
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
+        // using FutureBuilder so that the widget does not load until Firebase has initialized
+        body: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
           ),
 
-        // snapshot has status of the Future - none, waiting, active or done
-        builder: (context, snapshot)
-        {
-          switch (snapshot.connectionState)
-          {
-            case ConnectionState.done:
-              return Column(
-                children: [
-                  TextField(
-                    controller: _email,
-                    autocorrect: false,
-                    enableSuggestions: false,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                      hintText: "Email",
+          // snapshot has status of the Future - none, waiting, active or done
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                return Column(
+                  children: [
+                    TextField(
+                      controller: _email,
+                      autocorrect: false,
+                      enableSuggestions: false,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        hintText: "Email",
+                      ),
                     ),
-                  ),
-                  TextField(
-                    controller: _password,
-                    obscureText: true,
-                    autocorrect: false,
-                    enableSuggestions: false,
-                    decoration: const InputDecoration(
-                      hintText: "Password",
+                    TextField(
+                      controller: _password,
+                      obscureText: true,
+                      autocorrect: false,
+                      enableSuggestions: false,
+                      decoration: const InputDecoration(
+                        hintText: "Password",
+                      ),
                     ),
-                  ),
-                  TextButton(
-                    onPressed: () async
-                    {
-                      final email = _email.text;
-                      final password = _password.text;
+                    TextButton(
+                      onPressed: () async {
+                        final email = _email.text;
+                        final password = _password.text;
 
-                      // if a Future is returned, you need to use await to get the result
-                      final userAuth = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                          email: email,
-                          password: password
-                      );
+                        // if a Future is returned, you need to use await to get the result
+                        final userAuth = await FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                            email: email, password: password);
 
-                      print(userAuth);
-                    },
-                    child: const Text("Register"),
-                  ),
-                ],
-              );
-            default:
-              return const Text("Loading...");
-          }
-        },
-      )
+                        print(userAuth);
+                      },
+                      child: const Text("Register"),
+                    ),
+                  ],
+                );
+              default:
+                return const Text("Loading...");
+            }
+          },
+        )
     );
   }
 }
+
+
+//Creating a stateless homepage, but converted to Stateful later
+
 
