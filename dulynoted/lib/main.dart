0423d6505f6cbb838669b2396c9ea0df1d997dce
Syed Wafi Hasan <syedwafihasan@gmail.com>
2022-09-04 21:50:48 +0530

    Added new register_view.dart file and added exception handling

--- dulynoted/lib/main.dart
+++ dulynoted/lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:dulynoted/views/login_view.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'firebase_options.dart';
 
-void main()
-{
+void main() {
   //ensures Futures are initialized
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
@@ -17,105 +13,4 @@ void main()
     home: const LoginView(),
   ));
 }
-
-// creating Stateful RegisterView widget
-// this will be used as the base of our register view
-class RegisterView extends StatefulWidget
-{
-  const RegisterView({Key? key}) : super(key: key);
-
-  @override
-  State<RegisterView> createState() => _RegisterViewState();
-}
-
-class _RegisterViewState extends State<RegisterView>
-{
-  late final TextEditingController _email;
-  late final TextEditingController _password;
-
-  @override
-  void initState()
-  {
-    _email = TextEditingController();
-    _password = TextEditingController();
-
-    super.initState();
-  }
-
-  @override
-  void dispose()
-  {
-    _email.dispose();
-    _password.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context)
-  {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("Register User"),
-        ),
-
-        // using FutureBuilder so that the widget does not load until Firebase has initialized
-        body: FutureBuilder(
-          future: Firebase.initializeApp(
-            options: DefaultFirebaseOptions.currentPlatform,
-          ),
-
-          // snapshot has status of the Future - none, waiting, active or done
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.done:
-                return Column(
-                  children: [
-                    TextField(
-                      controller: _email,
-                      autocorrect: false,
-                      enableSuggestions: false,
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: const InputDecoration(
-                        hintText: "Email",
-                      ),
-                    ),
-                    TextField(
-                      controller: _password,
-                      obscureText: true,
-                      autocorrect: false,
-                      enableSuggestions: false,
-                      decoration: const InputDecoration(
-                        hintText: "Password",
-                      ),
-                    ),
-                    TextButton(
-                      onPressed: () async {
-                        final email = _email.text;
-                        final password = _password.text;
-
-                        // if a Future is returned, you need to use await to get the result
-                        final userAuth = await FirebaseAuth.instance
-                            .createUserWithEmailAndPassword(
-                            email: email,
-                            password: password
-                        );
-
-                        print(userAuth);
-                      },
-                      child: const Text("Register"),
-                    ),
-                  ],
-                );
-              default:
-                return const Text("Loading...");
-            }
-          },
-        )
-    );
-  }
-}
-
-
 //Creating a stateless homepage, but converted to Stateful later
-
-
--- dulynoted/lib/views/register_view.dart
+++ dulynoted/lib/views/register_view.dart
@@ -1,16 +1,19 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:dulynoted/views/login_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../firebase_options.dart';
 
-class LoginView extends StatefulWidget {
-  const LoginView({Key? key}) : super(key: key);
+// creating Stateful RegisterView widget
+// this will be used as the base of our register view
+class RegisterView extends StatefulWidget {
+  const RegisterView({Key? key}) : super(key: key);
 
   @override
-  State<LoginView> createState() => _LoginViewState();
+  State<RegisterView> createState() => _RegisterViewState();
 }
 
-class _LoginViewState extends State<LoginView> {
+class _RegisterViewState extends State<RegisterView> {
   late final TextEditingController _email;
   late final TextEditingController _password;
 
@@ -33,7 +36,7 @@ class _LoginViewState extends State<LoginView> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Login User"),
+          title: const Text("Register User"),
         ),
 
         // using FutureBuilder so that the widget does not load until Firebase has initialized
@@ -71,23 +74,24 @@ class _LoginViewState extends State<LoginView> {
                         final email = _email.text;
                         final password = _password.text;
 
-                        try // try block to handle exception if user does not exist
-                        {
-                          // if a Future is returned, you need to use await to get the result
+                        // if a Future is returned, you need to use await to get the result
+                        try {
                           final userAuth = await FirebaseAuth.instance
-                              .signInWithEmailAndPassword(
+                              .createUserWithEmailAndPassword(
                                   email: email, password: password);
                         } on FirebaseAuthException catch (e) {
-                          if (e.code == 'user-not-found') {
-                            print("User Not Found");
-                          } else if (e.code == 'wrong-password') {
-                            print("Password Incorrect");
+                          if (e.code == "email-already-in-use") {
+                            print("User already exists");
+                          } else if (e.code == "weak-password") {
+                            print("Password Too Weak");
+                          } else if (e.code == "invalid-email") {
+                            print("Email Invalid");
                           } else {
-                            print("Something else happened : ${e.code}");
+                            print("Caught exception : ${e.code}");
                           }
                         }
                       },
-                      child: const Text("Login"),
+                      child: const Text("Register"),
                     ),
                   ],
                 );
