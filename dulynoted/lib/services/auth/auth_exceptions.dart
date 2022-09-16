113a62888ec113799ec31264fd8e4e6b5d0fa859
Syed Wafi Hasan <syedwafihasan@gmail.com>
2022-09-16 22:44:22 +0530

    Added Auth layer files, TODO work in firebase_auth_provider.dart

---
+++ dulynoted/lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,15 @@
+// Login Exceptions
+
+class UserNotFoundAuthException implements Exception {}
+class WrongPasswordAuthException implements Exception {}
+
+// Registration exception
+
+class WeakPasswordAuthException implements Exception {}
+class EmailAlreadyInUseAuthException implements Exception {}
+class InvalidEmailAuthException implements Exception {}
+
+// Generic Exceptions
+
+class GenericAuthException implements Exception {}
+class UserNotLoggedInException implements Exception {}
