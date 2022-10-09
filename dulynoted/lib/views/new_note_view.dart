550f536711a3910f200ee5dbd9544c6665532849
Syed Wafi Hasan <syedwafihasan@gmail.com>
2022-10-09 20:12:45 +0530

    More changes

--- dulynoted/lib/views/new_note_view.dart
+++ dulynoted/lib/views/new_note_view.dart
@@ -1,3 +1,4 @@
+import 'package:dulynoted/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:dulynoted/services/crud/notes_service.dart';
 
@@ -19,7 +20,32 @@ class _NewNoteViewState extends State<NewNoteView> {
     if (existingNote != null) {
       return existingNote;
     }
+    final currentUser = AuthService.firebase().currentUser!;
+    final email = currentUser.email!;
+    final owner = await _notesService.getUser(email: email);
+    return await _notesService.createNote(owner: owner);
   }
+
+  void _deleteNoteIfTextIsEmpty() {
+    final note = _note;
+    if (_textEditingController.text.isEmpty && note != null) {
+      _notesService.deleteNote(id: note.id);
+    }
+  }
+
+  void _saveNoteIfTextIsNotEmpty() async {
+    final note = _note;
+    final text = _textEditingController.text;
+    if (note != null && text.isNotEmpty) {
+      await _notesService.updateNote(note: note, updatedText: text);
+    }
+  }
+
+  @override
+  void dispose() {
+
+  }
+
 
   @override
   Widget build(BuildContext context) {
